47351f15f41b7a3d4c2d9cfad65a4e9d669cffbd
josefinunefaldt <josefin6@hotmail.com>
2025-02-11 11:49:10 +0100

    Testcontroller for azure

---
+++ Api/Controllers/TestsController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok();
+        }
+    }
+}
