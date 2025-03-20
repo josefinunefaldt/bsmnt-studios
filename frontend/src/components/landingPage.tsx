7cf20cd97633e06ac10dddf493e6af95510ac32e
josefinunefaldt <josefin6@hotmail.com>
2025-03-20 10:20:46 +0100

    layout landing page

--- frontend/src/components/landingPage.tsx
+++ frontend/src/components/landingPage.tsx
@@ -2,48 +2,72 @@ import { Link } from "@tanstack/react-router";
 
 const LandingPage = () => {
   return (
-    <div className="w-full  mx-auto p-6">
+    <div className="w-full mx-auto p-6">
       <div className="flex flex-col lg:flex-row gap-2">
         <Link
           to="/argall"
-          className="w-full lg:w-1/2 flex items-center justify-center bg-gray-300 border border-black p-6 min-h-[300px]"
+          className="w-full lg:w-1/2 flex items-center justify-center min-h-[300px]"
         >
-          Argall way
+          <img
+            src="./Argall.jpeg"
+            className="object-cover w-full h-full"
+            alt="Argall"
+          />
         </Link>
         <Link
           to="/leabridge"
-          className="w-full lg:w-1/2 flex items-center justify-center bg-gray-300 border border-black p-6 min-h-[300px]"
+          className="w-full lg:w-1/2 flex items-center justify-center  min-h-[300px]"
         >
-          Lea bridge
+          <img
+            src="./Lea.jpg"
+            className="object-cover w-full h-full"
+            alt="Lea Bridge"
+          />
         </Link>
       </div>
 
       <div className="flex flex-col lg:flex-row gap-2 mt-2">
         <Link
           to="/hornsey"
-          className="w-full lg:w-1/2 flex items-center justify-center bg-gray-300 border border-black p-6 min-h-[300px]"
+          className="w-full lg:w-1/2 flex items-center justify-center min-h-[300px]"
         >
-          Hornsey
+          <img
+            src="./Finsbury.JPG"
+            className="object-cover w-full h-full"
+            alt="Hornsey"
+          />
         </Link>
         <Link
           to="/hackney"
-          className="w-full lg:w-1/2 flex items-center justify-center bg-gray-300 border border-black p-6 min-h-[300px]"
+          className="w-full lg:w-1/2 flex items-center justify-center  min-h-[300px]"
         >
-          Hackney
+          <img
+            src="./Hackney.jpg"
+            className="object-cover w-full h-full"
+            alt="Hackney"
+          />
         </Link>
       </div>
       <div className="flex flex-col lg:flex-row gap-2 mt-2">
         <Link
           to="/finsbury"
-          className="w-full lg:w-2/3 flex items-center justify-center bg-gray-300 border border-black p-6 min-h-[300px]"
+          className="w-full lg:w-2/3 flex items-center justify-center  min-h-[300px]"
         >
-          Finsbury
+          <img
+            src="./Hornsey.jpg"
+            className="object-cover w-full h-full"
+            alt="Finsbury"
+          />
         </Link>
         <Link
           to="/archway"
-          className="w-full lg:w-1/3 flex items-center justify-center bg-gray-300 border border-black p-6 min-h-[300px]"
+          className="w-full lg:w-1/3 flex items-center justify-center min-h-[300px]"
         >
-          Archway
+          <img
+            src="./Archway.JPG"
+            className="object-cover w-full h-full"
+            alt="Archway"
+          />
         </Link>
       </div>
     </div>
