37d76c2724f16a27a75a95a3584c069272759b42
josefinunefaldt <josefin6@hotmail.com>
2025-03-26 19:30:29 +0100

    fix location pages

--- frontend/src/routes/hackney.tsx
+++ frontend/src/routes/hackney.tsx
@@ -10,28 +10,28 @@ function RouteComponent() {
     <div>
       <Location
         content={[
-          { type: "image", value: "/Hackney/hackney1.jpg" },
-          { type: "image", value: "/Hackney/hackney2.jpg" },
+          { type: "image", value: "/Hackney/hackney6.jpg" },
+          { type: "image", value: "/Hackney/hackney3.jpg" },
           {
             type: "text",
             value:
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor Pellentesque vehicula risus at elit dignissim, a auctor augue auctor. Mauris nec risus nec odio fringilla aliquet. Nulla facilisi. Curabitur dictum malesuada augue, id consectetur felis vulputate a. Fusce in urna a libero dapibus lacinia. Duis varius suscipit risus, nec auctor nulla tincidunt eget. Vestibulum pharetra, nisl in efficitur pharetra, elit elit vestibulum nulla, ut accumsan purus ligula nec erat. Donec non facilisis justo. Sed a magna",
           },
-          { type: "image", value: "/Hackney/hackney3.jpg" },
+          { type: "image", value: "/Hackney/hackney2.jpg" },
           {
             type: "text",
             value:
               "Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctorLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctorLorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctorLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctorLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctorLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor risus at elit dignissim, a auctor vehicula risus at elit dignissim, a auctor Pellentesque vehicula risus at elit dignissim, a auctor adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor augue auctor. Mauris nec risus nec odio fringilla aliquet. Nulla facilisi. Curabitur dictum malesuada augue, id consectetur felis vulputate a. Fusce in urna a libero dapibus lacinia. Duis varius suscipit risus, nec auctor nulla tincidunt eget. Vestibulum pharetra, nisl in efficitur pharetra, elit elit vestibulum nulla, ut accumsan purus ligula nec erat. Donec non facilisis justo. Sed a magna",
           },
           { type: "image", value: "/Hackney/hackney4.jpg" },
-          { type: "image", value: "/Hackney/hackney5.jpg" },
+          { type: "image", value: "/Hackney/hackney7.jpg" },
           {
             type: "text",
             value:
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor augue auctor. Mauris nec risus nec odio fringilla aliquet. Nulla facilisi. Curabitur dictum malesuada augue, id consectetur felis vulputate a. Fusce in urna a libero dapibus lacinia. Duis varius suscipit risus, nec auctor nulla tincidunt eget. Vestibulum pharetra, nisl in efficitur pharetra, elit elit vestibulum nulla, ut accumsan purus ligula nec erat. Donec non facilisis justo. Sed a magna",
           },
-          { type: "image", value: "/Hackney/hackney6.jpg" },
-          { type: "image", value: "/Hackney/hackney7.jpg" },
+          { type: "image", value: "/Hackney/hackney1.jpg" },
+          { type: "image", value: "/Hackney/hackney5.jpg" },
           { type: "image", value: "/Hackney/hackney8.jpg" },
         ]}
       />
