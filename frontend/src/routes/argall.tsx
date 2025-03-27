00a9903f8267fc8f59c78bf7bf6178e65d9c799d
josefinunefaldt <josefin6@hotmail.com>
2025-03-27 09:46:14 +0100

    feat: video argall

--- frontend/src/routes/argall.tsx
+++ frontend/src/routes/argall.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Location from "../components/location";
 
 export const Route = createFileRoute("/argall")({
   component: RouteComponent,
@@ -7,34 +6,14 @@ export const Route = createFileRoute("/argall")({
 
 function RouteComponent() {
   return (
-    <div>
-      <Location
-        content={[
-          { type: "image", value: "/Archway/_MG_8921.JPG" },
-          { type: "image", value: "/Archway/_MG_8598.JPG" },
-          {
-            type: "text",
-            value:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor Pellentesque vehicula risus at elit dignissim, a auctor augue auctor. Mauris nec risus nec odio fringilla aliquet. Nulla facilisi. Curabitur dictum malesuada augue, id consectetur felis vulputate a. Fusce in urna a libero dapibus lacinia. Duis varius suscipit risus, nec auctor nulla tincidunt eget. Vestibulum pharetra, nisl in efficitur pharetra, elit elit vestibulum nulla, ut accumsan purus ligula nec erat. Donec non facilisis justo. Sed a magna",
-          },
-          { type: "image", value: "/Archway/_MG_8957.JPG" },
-          {
-            type: "text",
-            value:
-              "Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctorLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctorLorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctorLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctorLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctorLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor risus at elit dignissim, a auctor vehicula risus at elit dignissim, a auctor Pellentesque vehicula risus at elit dignissim, a auctor adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor augue auctor. Mauris nec risus nec odio fringilla aliquet. Nulla facilisi. Curabitur dictum malesuada augue, id consectetur felis vulputate a. Fusce in urna a libero dapibus lacinia. Duis varius suscipit risus, nec auctor nulla tincidunt eget. Vestibulum pharetra, nisl in efficitur pharetra, elit elit vestibulum nulla, ut accumsan purus ligula nec erat. Donec non facilisis justo. Sed a magna",
-          },
-          { type: "image", value: "/Archway/_MG_9145.JPG" },
-          { type: "image", value: "/Archway/_MG_9170.JPG" },
-          {
-            type: "text",
-            value:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor augue auctor. Mauris nec risus nec odio fringilla aliquet. Nulla facilisi. Curabitur dictum malesuada augue, id consectetur felis vulputate a. Fusce in urna a libero dapibus lacinia. Duis varius suscipit risus, nec auctor nulla tincidunt eget. Vestibulum pharetra, nisl in efficitur pharetra, elit elit vestibulum nulla, ut accumsan purus ligula nec erat. Donec non facilisis justo. Sed a magna",
-          },
-          { type: "image", value: "/Archway/1.jpg" },
-          { type: "image", value: "/Archway/Archway32.jpg" },
-          { type: "image", value: "/Archway/ArchwayTerrace.JPG" },
-        ]}
-      />
+    <div className="flex justify-center items-center min-h-screen p-4">
+      <video
+        controls
+        className="max-w-full max-h-screen rounded-lg shadow-lg"
+        src="/argallvideo.mp4"
+      >
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 }
