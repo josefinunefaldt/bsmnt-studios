28b19ee2c87a437f464a4af0cdb84e44cfbf52fe
josefinunefaldt <josefin6@hotmail.com>
2025-07-04 13:37:33 +0200

    fix location pages

--- frontend/src/components/locationCarousel.tsx
+++ frontend/src/components/locationCarousel.tsx
@@ -17,8 +17,8 @@ const LocationCarousel = () => {
       name: "Lea bridge",
       path: "/leabridge",
       location: "East London",
-      info: "Located in a landmark building by Lea Bridge Station, provides offices filled with natural light, spacious, inviting communal areas, and high-spec music studios with floor-to-ceiling windows for East London's creative professionals.",
-      image: "./l.jpg",
+      info: "Located in a landmark building by Lea Bridge Station, provides offices filled with natural light, spacious, inviting communal areas, and high-spec music studios with floor-to-ceiling windows for East London's creative professionals.",
+      image: "./Lea/lea.jpeg",
     },
     {
       name: "Argall",
@@ -32,28 +32,28 @@ const LocationCarousel = () => {
       location: "North London",
       info: "Provides spacious, welcoming studios flooded with natural light, alongside modern facilities and a relaxed vibe—set within a vibrant creative community close to local bars, cafés, restaurants and excellent transport links.",
       path: "/hornsey",
-      image: "./Hornsey.jpg",
+      image: "./Hornsey/hornsey1.jpg",
     },
     {
       name: "Archway",
       path: "/archway",
       location: "North London",
-      info: "Our original studios, nestled just steps away from Archway’s vibrant high street within a historic former WWII shelter, offer nine comfy, soundproofed production suites alongside a convenient break-out space with kitchenette and a private outdoor terrace.",
-      image: "./Archway.JPG",
+      info: "Our original studios, nestled just steps away from Archway's vibrant high street within a historic former WWII shelter, offer nine comfy, soundproofed production suites alongside a convenient break-out space with kitchenette and a private outdoor terrace.",
+      image: "./Archway/archway8.jpg",
     },
     {
       name: "Finsbury park",
       path: "/finsbury",
       location: "North London",
       info: "Once a textile factory, now warmly blends industrial charm with modern comfort, creating a friendly vibe and welcoming creative community known for its laid-back social gatherings—just the place to feel right at home.",
-      image: "./Finsbury/finsbury3.jpg",
+      image: "./Finsbury/finsbury1.jpg",
     },
     {
       name: "Hackney",
       path: "/hackney",
       location: "East London",
-      info: "Located in an old textile factory, Hackney BSMNT music studios combine tons of natural daylight, high ceilings, and a genuinely friendly, tight-knit creative community, just steps away from Hackney Downs park and some of the area’s best cafés and bars.",
-      image: "./hackneey.jpg",
+      info: "Located in an old textile factory, Hackney BSMNT music studios combine tons of natural daylight, high ceilings, and a genuinely friendly, tight-knit creative community, just steps away from Hackney Downs park and some of the area's best cafés and bars.",
+      image: "./Hackney/hackney15.jpg",
     },
   ];
 
@@ -90,7 +90,7 @@ const LocationCarousel = () => {
   return (
     <div className="w-full mx-auto relative bg-white">
       <div className="flex flex-col md:flex-row">
-        <div className="relative hidden md:block w-full md:w-3/4 h-[85vh] overflow-hidden">
+        <div className="w-full md:w-3/4 aspect-[16/9]">
           <Link
             to={currentLocation.path}
             className="block w-full h-full relative"
@@ -101,35 +101,34 @@ const LocationCarousel = () => {
               alt={currentLocation.name}
             />
           </Link>
-
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              prevSlide();
-            }}
-            className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-opacity-50 text-white p-4 rounded-full hover:bg-opacity-70 transition-all"
-            aria-label="Previous slide"
-          >
-            <MdKeyboardArrowLeft className="text-4xl" />
-          </button>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              nextSlide();
-            }}
-            className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-opacity-50 text-white p-4 rounded-full hover:bg-opacity-70 transition-all"
-            aria-label="Next slide"
-          >
-            <MdKeyboardArrowRight className="text-4xl" />
-          </button>
         </div>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            prevSlide();
+          }}
+          className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-opacity-50 text-white p-4 rounded-full hover:bg-opacity-70 transition-all"
+          aria-label="Previous slide"
+        >
+          <MdKeyboardArrowLeft className="text-4xl" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            nextSlide();
+          }}
+          className="absolute right-4 md:right-[26%] top-1/2 transform -translate-y-1/2 bg-opacity-50 text-white p-4 rounded-full hover:bg-opacity-70 transition-all"
+          aria-label="Next slide"
+        >
+          <MdKeyboardArrowRight className="text-4xl" />
+        </button>
 
         <div className="block md:hidden">
           {locations.map((location, index) => (
             <Link
               key={index}
               to={location.path}
-              className="relative block w-full mb-4"
+              className="relative block w-full mb-4 h-full"
             >
               <img
                 src={location.image}
@@ -145,22 +144,22 @@ const LocationCarousel = () => {
 
         <Link
           to={currentLocation.path}
-          className="hidden sm:block w-full md:w-1/4 h-[85vh] bg-[#fef880]  text-[rgb(67,67,67)] transition-all duration-500"
+          className="hidden md:block w-full md:w-1/4 aspect-[16/9] bg-[#fef880] text-[rgb(67,67,67)] transition-all duration-500"
         >
-          <div className="flex flex-col ml-7 m-auto mt-36 p-4 space-y-2">
-            <div className="text-4xl font-bold title ">
+          <div className="flex flex-col h-full justify-center px-7 py-8 space-y-4">
+            <div className="text-3xl font-bold title ">
               {currentLocation.name}
             </div>
 
-            <div className="text-base text-[rgb(67,67,67)] font-libre">
+            <div className="text-sm text-[rgb(67,67,67)] font-libre leading-relaxed">
               {currentLocation.info}
             </div>
-            <br />
-            <br />
-            <br />
-            <div className="font-libre text-[rgb(67,67,67)]">Read more</div>
-            <div className="left-0">
-              <img className="left-0 w-48" src="/Pil.png"></img>
+
+            <div className="font-libre text-[rgb(67,67,67)] mt-auto">
+              Read more
+            </div>
+            <div className="flex justify-start">
+              <img className="w-32" src="/Pil.png" alt="Arrow"></img>
             </div>
           </div>
         </Link>
--- frontend/src/routes/leabridge.tsx
+++ frontend/src/routes/leabridge.tsx
@@ -14,15 +14,39 @@ function RouteComponent() {
       <Location2
         isPortrait={isLeaPortrait}
         content={[
-          { type: "image", value: "/Lea/lea8.jpg" },
           {
             type: "image",
             value: "/Lea/lea2.jpg",
           },
           {
             type: "text",
+            value: (
+              <div className="flex flex-col">
+                <h2 className="text-2xl font-bold mb-2">LEA BRIDGE</h2>
+
+                <p>
+                  Lorem ipsum dolor sit amet, consectetunsectetur adipiscing
+                  elit. Pellentesque vehicula risus at elit dignissim, a auctor
+                  Pellentesque vehicula risus at elit dignissim, a auctor augue
+                  auctor. Mauris nec risus nec odio fringilla aliquet. Nulla
+                  facilisi. Curabitur dictum malesuada augue, id consectetur
+                  felis vulputate a. Fusce in urna a libero dapibus lacinia.
+                  Duis varius suscipit risus, nec auctor nulla tincidunt eget.
+                  Vestibulum pharetra, nisl in efficitur pharetra, elit elit
+                  vestibulum nulla, ut accumsan purus ligula nec erat. Donec non
+                  facilisis justo. Sed a magna
+                </p>
+              </div>
+            ),
+          },
+          {
+            type: "image",
+            value: "/Lea/lea4.jpeg",
+          },
+          {
+            type: "text",
             value:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor Pellentesque vehicula risus at elit dignissim, a auctor augue auctor. Mauris nec risus nec odio fringilla aliquet. Nulla facilisi. Curabitur dictum malesuada augue, id consectetur felis vulputate a. Fusce in urna a libero dapibus lacinia. Duis varius suscipit risus, nec auctor nulla tincidunt eget. Vestibulum pharetra, nisl in efficitur pharetra, elit elit vestibulum nulla, ut accumsan purus ligula nec erat. Donec non facilisis justo. Sed a magna",
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor augue auctor. Mauris nec risus nec odio fringilla aliquet. Nulla facilisi. Curabitur dictum malesuada augue, id consectetur felis vulputate a. Fusce in urna a libero dapibus lacinia. Duis varius suscipit risus, nec auctor nulla tincidunt eget. Vestibulum pharetra, nisl in efficitur pharetra, elit elit vestibulum nulla, ut accumsan purus ligula nec erat. Donec non facilisis justo. Sed a magna",
           },
           {
             type: "image",
@@ -33,28 +57,17 @@ function RouteComponent() {
             value:
               "Lorem ipsum dolor sit amet, consectetunsectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor Pellentesque vehicula risus at elit dignissim, a auctor augue auctor. Mauris nec risus nec odio fringilla aliquet. Nulla facilisi. Curabitur dictum malesuada augue, id consectetur felis vulputate a. Fusce in urna a libero dapibus lacinia. Duis varius suscipit risus, nec auctor nulla tincidunt eget. Vestibulum pharetra, nisl in efficitur pharetra, elit elit vestibulum nulla, ut accumsan purus ligula nec erat. Donec non facilisis justo. Sed a magna",
           },
-          {
-            type: "image",
-            value: "/Lea/lea7.jpeg",
-          },
-          {
-            type: "image",
-            value: "/Lea/lea3.jpeg",
-          },
-          {
-            type: "text",
-            value:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor augue auctor. Mauris nec risus nec odio fringilla aliquet. Nulla facilisi. Curabitur dictum malesuada augue, id consectetur felis vulputate a. Fusce in urna a libero dapibus lacinia. Duis varius suscipit risus, nec auctor nulla tincidunt eget. Vestibulum pharetra, nisl in efficitur pharetra, elit elit vestibulum nulla, ut accumsan purus ligula nec erat. Donec non facilisis justo. Sed a magna",
-          },
-          {
-            type: "image",
-            value: "/Lea/lea4.jpeg",
-          },
+          { type: "image", value: "/Lea/lea8.jpg" },
+
           {
             type: "image",
             value: "/Lea/lea6.jpeg",
           },
-          { type: "image", value: "/Lea/lea1.jpeg" },
+          {
+            type: "text",
+            value:
+              "Lorem ipsum dolor sit amet, consectetunsectetur adipiscing elit. Pellentesque vehicula risus at elit dignissim, a auctor Pellentesque vehicula risus at elit dignissim, a auctor augue auctor. Mauris nec risus nec odio fringilla aliquet. Nulla facilisi. Curabitur dictum malesuada augue, id consectetur felis vulputate a. Fusce in urna a libero dapibus lacinia. Duis varius suscipit risus, nec auctor nulla tincidunt eget. Vestibulum pharetra, nisl in efficitur pharetra, elit elit vestibulum nulla, ut accumsan purus ligula nec erat. Donec non facilisis justo. Sed a magna",
+          },
         ]}
       />
     </div>
